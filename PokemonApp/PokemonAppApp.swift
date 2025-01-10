00632315b377822d14b9f1ee2d5c3ff4765f9d2e
Abdul Mateen <amateen@sastaticket.pk>
2025-01-10 15:05:36 +0500

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Abdul Mateen on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokemonAppTests/PokemonAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppTests.swift
+//  PokemonAppTests
+//
+//  Created by Abdul Mateen on 10/01/2025.
+//
+
+import Testing
+@testable import PokemonApp
+
+struct PokemonAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
